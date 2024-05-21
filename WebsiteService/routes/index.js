caeb08f4c7f651e48eae41af0b3d9c712bf9f80c
Spazmann <dmann@student.neumont.edu>
2024-05-21 09:37:47 -0600

    Login is required

--- WebsiteService/routes/index.js
+++ WebsiteService/routes/index.js
@@ -3,45 +3,52 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  const dmList = [
-    { name: 'Hiroshima Vault Dwellers', members: '10 Members' },
-    { name: 'Hiroshima Vault Dwellers', members: '10 Members' },
-    { name: 'Hiroshima Vault Dwellers', members: '10 Members' },
-    { name: 'Hiroshima Vault Dwellers', members: '10 Members' },
-    { name: 'Hiroshima Vault Dwellers', members: '10 Members' },
-    { name: 'Hiroshima Vault Dwellers', members: '10 Members' },
-    { name: 'Hiroshima Vault Dwellers', members: '10 Members' },
-    { name: 'Hiroshima Vault Dwellers', members: '10 Members' },
-    { name: 'Hiroshima Vault Dwellers', members: '10 Members' },
-    { name: 'Hiroshima Vault Dwellers', members: '10 Members' },
-    { name: 'Hiroshima Vault Dwellers', members: '10 Members' },
-    { name: 'Hiroshima Vault Dwellers', members: '10 Members' },
-    { name: 'Hiroshima Vault Dwellers', members: '10 Members' },
-    { name: 'Hiroshima Vault Dwellers', members: '10 Members' },
-    { name: 'Hiroshima Vault Dwellers', members: '10 Members' },
-    { name: 'Hiroshima Vault Dwellers', members: '10 Members' },
-    { name: 'Hiroshima Vault Dwellers', members: '10 Members' },
-    { name: 'Hiroshima Vault Dwellers', members: '10 Members' },
-    { name: 'Hiroshima Vault Dwellers', members: '10 Members' },
-    { name: 'Hiroshima Vault Dwellers', members: '10 Members' },
-    { name: 'Hiroshima Vault Dwellers', members: '10 Members' },
-    { name: 'Hiroshima Vault Dwellers', members: '10 Members' },
-    { name: 'Hiroshima Vault Dwellers', members: '10 Members' },
-    { name: 'Hiroshima Vault Dwellers', members: '10 Members' },
-    { name: 'Hiroshima Vault Dwellers', members: '10 Members' }
-  ];
-  const messsageList = [
-    { username: 'Spazmann', message:'Hello this is Dimtri Mann'},
-    { username: 'Spazmann', message:'Hello this is Dimtri Mann'},
-    { username: 'Spazmann', message:'Hello this is Dimtri Mann Hello this is Dimtri Mann Hello this is Dimtri Mann Hello this is Dimtri Mann Hello this is Dimtri Mann Hello this is Dimtri Mann Hello this is Dimtri Mann Hello this is Dimtri Mann Hello this is Dimtri Mann Hello this is Dimtri Mann Hello this is Dimtri Mann'},
-    { username: 'Spazmann', message:'Hello this is Dimtri Mann'},
-    { username: 'Spazmann', message:'Hello this is Dimtri Mann'},
-    { username: 'Spazmann', message:'Hello this is Dimtri Mann'},
-    { username: 'Spazmann', message:'Hello this is Dimtri Mann'},
-    { username: 'Spazmann', message:'Hello this is Dimtri Mann'},
-    { username: 'Spazmann', message:'Hello this is Dimtri Mann'}
-  ]
-  res.render('index', { title: 'Home', dmList, messsageList });
+  const user = {Id:"3892132138210983021", Username:"Spazmann", Email:"email@gmail.com"}
+  //const user = null
+  if (!user) {
+    res.render('login', { title: 'Login' });
+  } else {
+
+    const dmList = [
+      { name: 'Hiroshima Vault Dwellers', members: '10 Members' },
+      { name: 'Hiroshima Vault Dwellers', members: '10 Members' },
+      { name: 'Hiroshima Vault Dwellers', members: '10 Members' },
+      { name: 'Hiroshima Vault Dwellers', members: '10 Members' },
+      { name: 'Hiroshima Vault Dwellers', members: '10 Members' },
+      { name: 'Hiroshima Vault Dwellers', members: '10 Members' },
+      { name: 'Hiroshima Vault Dwellers', members: '10 Members' },
+      { name: 'Hiroshima Vault Dwellers', members: '10 Members' },
+      { name: 'Hiroshima Vault Dwellers', members: '10 Members' },
+      { name: 'Hiroshima Vault Dwellers', members: '10 Members' },
+      { name: 'Hiroshima Vault Dwellers', members: '10 Members' },
+      { name: 'Hiroshima Vault Dwellers', members: '10 Members' },
+      { name: 'Hiroshima Vault Dwellers', members: '10 Members' },
+      { name: 'Hiroshima Vault Dwellers', members: '10 Members' },
+      { name: 'Hiroshima Vault Dwellers', members: '10 Members' },
+      { name: 'Hiroshima Vault Dwellers', members: '10 Members' },
+      { name: 'Hiroshima Vault Dwellers', members: '10 Members' },
+      { name: 'Hiroshima Vault Dwellers', members: '10 Members' },
+      { name: 'Hiroshima Vault Dwellers', members: '10 Members' },
+      { name: 'Hiroshima Vault Dwellers', members: '10 Members' },
+      { name: 'Hiroshima Vault Dwellers', members: '10 Members' },
+      { name: 'Hiroshima Vault Dwellers', members: '10 Members' },
+      { name: 'Hiroshima Vault Dwellers', members: '10 Members' },
+      { name: 'Hiroshima Vault Dwellers', members: '10 Members' },
+      { name: 'Hiroshima Vault Dwellers', members: '10 Members' }
+    ];
+    const messsageList = [
+      { username: 'Spazmann', message:'Hello this is Dimtri Mann'},
+      { username: 'Spazmann', message:'Hello this is Dimtri Mann'},
+      { username: 'Spazmann', message:'Hello this is Dimtri Mann Hello this is Dimtri Mann Hello this is Dimtri Mann Hello this is Dimtri Mann Hello this is Dimtri Mann Hello this is Dimtri Mann Hello this is Dimtri Mann Hello this is Dimtri Mann Hello this is Dimtri Mann Hello this is Dimtri Mann Hello this is Dimtri Mann'},
+      { username: 'Spazmann', message:'Hello this is Dimtri Mann'},
+      { username: 'Spazmann', message:'Hello this is Dimtri Mann'},
+      { username: 'Spazmann', message:'Hello this is Dimtri Mann'},
+      { username: 'Spazmann', message:'Hello this is Dimtri Mann'},
+      { username: 'Spazmann', message:'Hello this is Dimtri Mann'},
+      { username: 'Spazmann', message:'Hello this is Dimtri Mann'}
+    ]
+    res.render('index', { title: 'Home', dmList, messsageList, user });
+  }
 });
 
 module.exports = router;
