4a5d30b425d0cee324edbb39952d6279ba3a81aa
Spazmann <dmann@student.neumont.edu>
2024-05-27 20:06:35 -0600

    Too much

--- WebsiteService/public/javascripts/index.js
+++ WebsiteService/public/javascripts/index.js
@@ -40,7 +40,7 @@ document.addEventListener('DOMContentLoaded', function () {
       messageItem.className = 'message-item';
       messageItem.innerHTML = `
         <div class="avatar">
-          <img src="https://via.placeholder.com/40" alt="User Avatar">
+          <img src="https://t4.ftcdn.net/jpg/00/65/77/27/360_F_65772719_A1UV5kLi5nCEWI0BNLLiFaBPEkUbv5Fv.jpg" alt="User Avatar">
         </div>
         <div class="message-info">
           <div class="message-stats">
